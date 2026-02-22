0977713510fabff07d1167243dcdb9ff26586a73
diegolinero <152449998+diegolinero@users.noreply.github.com>
2026-02-22 13:06:24 -0300

    Fix duplicate model typealias collisions affecting kapt

--- app/src/main/java/com/astralimit/dogfit/WeeklySummary.kt
+++ app/src/main/java/com/astralimit/dogfit/WeeklySummary.kt
@@ -1,4 +1,3 @@
 package com.astralimit.dogfit
 
-typealias WeeklySummaryModel = com.astralimit.dogfit.model.WeeklySummaryModel
 typealias WeeklySummary = com.astralimit.dogfit.model.WeeklySummaryModel
